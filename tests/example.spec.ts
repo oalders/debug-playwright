ee5bae7610bc7b4b301d990df3dd66de1dc3e278
Olaf Alders <olaf@wundersolutions.com>
2024-01-12 20:07:24 -0500

    use asciify-image

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { dumpFormattedContent, printScreenshot } from '../lib/debug';
+import { dumpFormattedContent, printScreenshot, Using } from '../lib/debug';
 
 test('2xx', async ({ page }) => {
     dumpFormattedContent(page);
@@ -31,8 +31,28 @@ test('2xx png', async ({ page }) => {
     await expect(page).not.toHaveTitle('title');
 });
 
-test('2xx screenshot', async ({ page }) => {
+test('2xx ASCII png', async ({ page }) => {
+    dumpFormattedContent(page, Using.ascii);
+    await page.goto('https://vilerichard.com/static/photos/group1.jpg');
+
+    // Expect a title not "to contain" a substring.
+    await expect(page).not.toHaveTitle('title');
+});
+
+test('2xx screenshot default', async ({ page }) => {
     await page.goto('https://example.com');
     await printScreenshot(page);
+    await expect(page).toHaveTitle(/Example Domain/);
+});
+
+test('2xx screenshot wezterm imgcat', async ({ page }) => {
+    await page.goto('https://example.com');
+    await printScreenshot(page);
+    await expect(page).toHaveTitle(/Example Domain/);
+});
+
+test('2xx ASCII screenshot', async ({ page }) => {
+    await page.goto('https://example.com');
+    await printScreenshot(page, Using.ascii)
     await expect(page).toHaveTitle(/Example Domain/);
 });
