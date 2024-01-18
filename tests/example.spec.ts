9b9f3c717ad99b3ffd3faf1fe91b4d04e4e6a21f
Olaf Alders <olaf@wundersolutions.com>
2024-01-18 20:54:58 +0000

    tidy

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,61 +1,69 @@
 import { test, expect } from '@playwright/test';
-import { dumpFormattedContent, printScreenshot, Using } from '../lib/debug';
+import { dumpFormattedContent, logResponse, printScreenshot, Using } from '../lib/debug';
 
 test('2xx', async ({ page }) => {
-    dumpFormattedContent(page);
-    await page.goto('https://example.com');
+  dumpFormattedContent(page);
+  await page.goto('https://example.com');
 
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Example Domain/);
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Example Domain/);
+});
+
+test('2xx logResponse', async ({ page }) => {
+  logResponse(page);
+  await page.goto('https://example.com');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Example Domain/);
 });
 
 test('4xx', async ({ page }) => {
-    dumpFormattedContent(page);
-    await page.goto('https://example.com/404');
+  logResponse(page);
+  await page.goto('https://example.com/404');
 
-    await expect(page).toHaveTitle(/Example Domain/);
+  await expect(page).toHaveTitle(/Example Domain/);
 });
 
 test('2xx JSON', async ({ page }) => {
-    test.skip(process.env.CI === "true", 'do not test wezterm under CI');
-    dumpFormattedContent(page);
-    await page.goto('https://filesamples.com/samples/code/json/sample1.json');
+  test.skip(process.env.CI === "true", 'do not test wezterm under CI');
+  dumpFormattedContent(page);
+  await page.goto('https://filesamples.com/samples/code/json/sample1.json');
 
-    await expect(page).not.toHaveTitle('title');
+  await expect(page).not.toHaveTitle('title');
 });
 
 test('2xx png', async ({ page }) => {
-    test.skip(process.env.CI === "true", 'do not test wezterm under CI');
-    dumpFormattedContent(page);
-    await page.goto('https://vilerichard.com/static/photos/group1.jpg');
+  test.skip(process.env.CI === "true", 'do not test wezterm under CI');
+  dumpFormattedContent(page);
+  await page.goto('https://vilerichard.com/static/photos/group1.jpg');
 
-    await expect(page).not.toHaveTitle('title');
+  await expect(page).not.toHaveTitle('title');
 });
 
 test('2xx screenshot default', async ({ page }) => {
-    test.skip(process.env.CI === "true", 'do not test wezterm under CI');
-    await page.goto('https://example.com');
-    await printScreenshot(page);
-    await expect(page).toHaveTitle(/Example Domain/);
+  test.skip(process.env.CI === "true", 'do not test wezterm under CI');
+  await page.goto('https://example.com');
+  await printScreenshot(page);
+  await expect(page).toHaveTitle(/Example Domain/);
 });
 
 test('2xx screenshot wezterm imgcat', async ({ page }) => {
-    test.skip(process.env.CI === "true", 'do not test wezterm under CI');
-    await page.goto('https://example.com');
-    await printScreenshot(page);
-    await expect(page).toHaveTitle(/Example Domain/);
+  test.skip(process.env.CI === "true", 'do not test wezterm under CI');
+  await page.goto('https://example.com');
+  await printScreenshot(page);
+  await expect(page).toHaveTitle(/Example Domain/);
 });
 
 test('2xx ASCII screenshot', async ({ page }) => {
-    await page.goto('https://example.com');
-    await printScreenshot(page, Using.ascii)
-    await expect(page).toHaveTitle(/Example Domain/);
+  await page.goto('https://example.com');
+  await printScreenshot(page, Using.ascii)
+  await expect(page).toHaveTitle(/Example Domain/);
 });
 
 test('2xx ASCII png', async ({ page }) => {
-    dumpFormattedContent(page, Using.ascii);
-    await page.goto('https://vilerichard.com/static/photos/group1.jpg');
+  dumpFormattedContent(page, Using.ascii);
+  await page.goto('https://vilerichard.com/static/photos/group1.jpg');
 
-    // Expect a title not "to contain" a substring.
-    await expect(page).not.toHaveTitle('title');
+  // Expect a title not "to contain" a substring.
+  await expect(page).not.toHaveTitle('title');
 });
