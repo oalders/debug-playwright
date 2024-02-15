234b8e0dec79c8bee204a9dc2c8add7388c12bc5
Olaf Alders <olaf@wundersolutions.com>
2024-02-15 17:07:58 -0500

    Prepare for npm publishing

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { DebugPlaywright, afterEachHandler } from '../lib/debug';
+import { DebugPlaywright, afterEachHandler } from '../lib';
 
 if (process.env.CI === 'true') {
   test.afterEach(async ({ page }, testInfo) => {
