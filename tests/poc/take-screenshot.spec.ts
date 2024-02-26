ab9887e9eff56d4f063040807540c3c620b36914
Olaf Alders <olaf@wundersolutions.com>
2024-02-25 23:49:14 -0500

    Add take screenshot poc

---
+++ tests/poc/take-screenshot.spec.ts
@@ -0,0 +1,6 @@
+import { test } from '@playwright/test';
+
+test('2xx', async ({ page }) => {
+  await page.goto('https://example.com');
+  await page.screenshot({path: '/tmp/screenshot.png'});
+});
