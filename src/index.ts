ca37e964fb58b7d8cdd34a142c2afaed295d2c0d
Olaf Alders <olaf@wundersolutions.com>
2024-02-26 00:22:37 -0500

    Refactor lynx command into function

--- src/index.ts
+++ src/index.ts
@@ -182,26 +182,7 @@ export class DebugPlaywright {
       return;
     }
 
-    const text = await response.text();
-
-    const child = spawn('lynx', ['-stdin', '-dump']);
-
-    child.stdin.write(text);
-    child.stdin.end();
-
-    child.stdout.setEncoding('utf8');
-
-    child.stdout.on('data', (data) => {
-      console.log(data);
-    });
-
-    child.on('error', (error) => {
-      console.error(`Error from child process: ${error}`);
-    });
-
-    child.stdout.on('end', () => {
-      console.log('Child process ended');
-    });
+    executeLynxCommand(await response.text());
   };
 
   handleRequestEvent = async (data: any, eventName: string) => {
@@ -230,3 +211,24 @@ export class DebugPlaywright {
     }
   };
 }
+
+const executeLynxCommand = (text: string) => {
+  const child = spawn('lynx', ['-stdin', '-dump']);
+
+  child.stdin.write(text);
+  child.stdin.end();
+
+  child.stdout.setEncoding('utf8');
+
+  child.stdout.on('data', (data) => {
+    console.log(data);
+  });
+
+  child.on('error', (error) => {
+    console.error(`Error from child process: ${error}`);
+  });
+
+  child.stdout.on('end', () => {
+    console.log('Child process ended');
+  });
+};
