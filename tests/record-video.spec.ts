12169c23b72eff049a22f6bc5189f7ad9c3c8c2d
Olaf Alders <olaf@wundersolutions.com>
2024-02-27 17:40:20 -0500

    Skip recording test in CI

--- tests/record-video.spec.ts
+++ tests/record-video.spec.ts
@@ -5,6 +5,9 @@ test.afterEach(afterEachHandler());
 test.use({ video: 'on' });
 
 test('record video', async ({ page }) => {
+  if (process.env.CI === 'true') {
+    test.skip();
+  }
   await page.goto('https://www.olafalders.com');
   await page.getByRole('link', { name: 'About Me' }).nth(0).click();
   await page.getByRole('link', { name: 'Feed' }).click();
